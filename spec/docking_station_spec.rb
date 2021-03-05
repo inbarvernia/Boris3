27d0c193c0406bcde5ebf7e8d64ac7a7e8d71aa8
inbarvernia <78118133+inbarvernia@users.noreply.github.com>
2021-03-05 11:58:22 +0000

    Refactored docking_station_spec using context

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,12 +7,17 @@ describe DockingStation do
   bike = Bike.new
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
-    it 'releases a bike' do
-      subject.dock(bike)
-      expect(subject.release_bike).to respond_to :working?
+    context 'when a bike is available' do
+      it 'releases a bike' do
+        subject.dock(bike)
+        expect(subject.release_bike).to respond_to :working?
+      end
     end
-    it 'raises an error if no bikes are available' do
-      expect {subject.release_bike}.to raise_error("Error: No bikes available")
+    context 'when no bikes are available' do
+      it 'raises an error' do
+        subject.bike_docked == nil
+        expect {subject.release_bike}.to raise_error("Error: No bikes available")
+      end
     end
   end
   it { is_expected.to respond_to(:dock).with(1).argument }
